1038067765dca8532c00eb9f01d5e1516cd91eb2
dev-hojaelee <hoje0711@naver.com>
2021-12-14 16:30:04 +0900

    django course 14 commit

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False) #char이 null이 되도 괜찮은지. 안괜찮으니 false.
