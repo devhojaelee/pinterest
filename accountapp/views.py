6904378b408cc295820805b68e41c741e6c290d5
dev-hojaelee <hoje0711@naver.com>
2021-12-15 17:21:48 +0900

    django course 21 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -19,3 +22,13 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list':hello_world_list})
+
+class AccountCreateView(CreateView):
+    model = User #상속
+    form_class = UserCreationForm #장고가 기본제공
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
+
+
+    #함수와 클래스의 불러오는 방식이 달라서, reverse를 그대로 사용할 수 없다.
+    #reverse_lazy는 클래스 뷰에서 사용하고 reverse는 함수 뷰에서 사용한다.
